286a68d61449c74d46fba59e86507b3bc1351698
Sapna Rawat <sapnarawat@TTNs-MacBook-Pro.local>
2022-10-10 17:03:40 +0530

    Edited changes

--- App.js
+++ App.js
@@ -1,60 +1,67 @@
 import React, { Component } from "react";
-import { View,SafeAreaView,Text,Button, StyleSheet} from "react-native";
+import { View, SafeAreaView, Text, Button, StyleSheet, TouchableOpacity } from "react-native";
 
 class App extends React.Component {
-  constructor(){
+  constructor() {
     super();
-    this.state={
-counter:  0
+    this.state = {
+      counter: 0
     }
   }
-  increment=()=>{
+  increment = () => {
 
-    this.setState({counter:this.state.counter  +1})
+    this.setState({ counter: this.state.counter + 1 })
   }
-  decrement=()=>{
-    if (this.state.counter>0){
-      this.setState({counter:this.state.counter  -1})
+  decrement = () => {
+    if (this.state.counter > 0) {
+      this.setState({ counter: this.state.counter - 1 })
 
     }
   }
   render() {
     return (
       <SafeAreaView>
-      <View>
-      <Text style={styles.textstyle}>{this.state.counter}</Text>
-
-      <Button onPress={this.increment} title="+"/>
-      <Button style={styles.commonbutton} onPress={this.decrement} title="-"/>
-      </View>
+        <View style={styles.wrap}>
+          <Text style={{ fontSize: 40 }}>Counter</Text>
+          <View style={styles.counter}>
+            <TouchableOpacity style={styles.button} onPress={this.increment}>
+              <Text style={styles.btnText}>+</Text>
+            </TouchableOpacity>
+            <Text style={{ fontSize: 70 }}>{this.state.counter}</Text>
+            <TouchableOpacity style={styles.button} onPress={this.decrement}>
+              <Text style={styles.btnText}>-</Text>
+            </TouchableOpacity>
+          </View>
+          <TouchableOpacity style={styles.button} onPress={this.Reset}>
+            <Text style={styles.btnText}>Reset</Text>
+          </TouchableOpacity>
+        </View>
       </SafeAreaView>
     );
   }
 }
-const styles = StyleSheet.create({
-  textstyle:{
-    fontSize:60, 
-     marginBottom:-20, 
-     textAlign:'center',
-     width:"100%",
-     marginTop:"40",
-     marginBottom:"40",
-  },
-  // button: {
-  //   alignItems: 'center',
-  //   justifyContent: 'center',
-  //   paddingVertical: 12,
-  //   paddingHorizontal: 32,
-  //   borderRadius: 4,
-  //   elevation: 3,
-  //   backgroundColor: 'black',
-  //   fontSize: 16,
-  //   lineHeight: 21,
-  //   fontWeight: 'bold',
-  //   letterSpacing: 0.25,
-  //   color: 'white',
-  // },
-  
 
+const styles = StyleSheet.create({
+  counter: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: 'blue',
+    padding: 10,
+    margin: 40,
+    borderRadius: 10
+  },
+  wrap: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  btnText: {
+    color: 'white'
+  }
 });
+
 export default App;
