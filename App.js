4fa71f17c5ec62165b4bec7ae397a98b618932d7
sapnar2001 <rawatsapna0622@gmail.com>
2022-10-11 10:29:52 +0530

    updateeee

--- App.js
+++ App.js
@@ -1,22 +1,53 @@
 import React, { Component } from "react";
 import { View, SafeAreaView, Text, Button, StyleSheet, TouchableOpacity } from "react-native";
 
+
 class App extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-      counter: 0
-    }
+      count: 0,
+    };
+    console.log('constructor called');
   }
+
   increment = () => {
-
-    this.setState({ counter: this.state.counter + 1 })
-  }
-  decrement = () => {
-    if (this.state.counter > 0) {
-      this.setState({ counter: this.state.counter - 1 })
-
+    if (this.state.count < 10) {
+      this.setState({count: this.state.count + 1});
     }
+  };
+
+  decrement = () => {
+    if (this.state.count > 0) {
+      this.setState({count: this.state.count - 1});
+    }
+  };
+
+  static getDerivedStateFromProps() {
+    console.log('Parent getDerivedStateFromProps called');
+    return null;
+  }
+
+  componentDidMount() {
+    console.log('Parent componentDidMount called');
+  }
+
+  componentDidUpdate() {
+    console.log('Parent componentDidUpdate called');
+  }
+
+  shouldComponentUpdate() {
+    console.log('Parent shouldComponentUpdate called');
+    return true;
+  }
+
+  getSnapshotBeforeUpdate() {
+    console.log('Parent getSnapshotBeforeUpdate called');
+    return null;
+  }
+
+  componentWillUnmount() {
+    console.log('Parent componentWillUnmount');
   }
   render() {
     return (
